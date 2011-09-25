b4d5ae5946aeec470a779f3ada6aa58604d6950c
nikhilneela <nikhilneela@gmail.com>
2011-09-25 22:45:38 +0530

    changed import statements

--- testApp/src/com/iiitb/nikhil/testApp/TestAppActivity.java
+++ testApp/src/com/iiitb/nikhil/testApp/TestAppActivity.java
@@ -1,18 +1,7 @@
 package com.iiitb.nikhil.testApp;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
+
 import java.util.ArrayList;
 
 import android.app.Activity;
